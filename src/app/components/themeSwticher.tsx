a4bf5d3e93e8d80a5125f4f276c85c12bfc835d6
Keyyard <keyyard8888@gmail.com>
2024-12-01 23:53:03 +0700

    Refactor theme management in ThemeSwitcher component to remove localStorage dependency and streamline meta tag updates

--- src/app/components/themeSwticher.tsx
+++ src/app/components/themeSwticher.tsx
@@ -2,23 +2,20 @@ import { Moon, Sun } from "lucide-react";
 import { useEffect, useMemo, useState } from "react";
 
 export default function ThemeSwitcher() {
-  // Check localStorage for a saved theme or default to light
-  const [theme, setTheme] = useState(() => localStorage.getItem('theme') || "light");
+  const [theme, setTheme] = useState("light");
 
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
     const metaThemeColor = document.querySelector("meta[name=theme-color]");
     const metaAppleStatusBarStyle = document.querySelector("meta[name=apple-mobile-web-app-status-bar-style]");
-
-    // Dynamically change the meta theme color and Apple status bar style
+    
     if (metaThemeColor) {
       metaThemeColor.setAttribute("content", theme === "light" ? "#ffffff" : "#000000");
     }
-
+    
     if (metaAppleStatusBarStyle) {
       metaAppleStatusBarStyle.setAttribute("content", theme === "light" ? "default" : "black-translucent");
     }
-    localStorage.setItem('theme', theme);
   }, [theme]);
 
   const toggleTheme = () => {
@@ -30,11 +27,7 @@ export default function ThemeSwitcher() {
 
   return (
     <div className="p-4">
-      <Icon
-        className={`text-${theme === "light" ? "black" : "white"} cursor-pointer`}
-        size={24}
-        onClick={toggleTheme}
-      />
+      <Icon className={`text-${theme === "light" ? "black" : "white"} cursor-pointer`} size={24} onClick={toggleTheme} />
     </div>
   );
 }
