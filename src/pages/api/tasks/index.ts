758ae27170061274e3f8eb0cc19e76db464069c7
Keyyard <keyyard8888@gmail.com>
2024-12-01 23:57:42 +0700

    Change task retrieval order to descending by date

--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -44,7 +44,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       try {
         const tasks = await prisma.task.findMany({
           where: { userId: user.id },
-          orderBy: { date: 'asc' },
+          orderBy: { date: 'desc' },
         });
         return res.status(200).json({ tasks });
       } catch (error) {
