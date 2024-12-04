697db940edabdfcf92219c3b971282cef2922470
Keyyard <keyyard8888@gmail.com>
2024-12-04 14:56:23 +0700

    Remove redundant social media meta tags and streamline layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,53 +74,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        {/* Social Media Meta Tags */}
-        {/* Open Graph (OG) for Facebook, LinkedIn, etc. */}
-        <meta property="og:title" content="Today Notes" />
-        <meta
-          property="og:description"
-          content="Today Notes is a lightweight, daily task management web app with task expiration, light/dark modes, and real-time notifications."
-        />
-        <meta property="og:url" content="https://today-notes.keyyard.xyz/" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:image"
-          content="https://today-notes.keyyard.xyz/icons/180.png"
-        />
-        <meta property="og:site_name" content="Today Notes" />
-
-        {/* Twitter Card for better sharing on Twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Today Notes" />
-        <meta
-          name="twitter:description"
-          content="Lightweight, daily task management web app with task expiration, light/dark modes, and real-time notifications."
-        />
-        <meta
-          name="twitter:image"
-          content="https://today-notes.keyyard.xyz/icons/180.png"
-        />
-        <meta name="twitter:creator" content="@keyyard" />
-
-        {/* Web App Meta Tags */}
-        <meta name="viewport" content={viewport} />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-title" content="Today Notes" />
-        {/* Light theme status bar */}
-        <meta name="theme-color" content="#ffffff" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
-        {/* Manifest */}
-        <link rel="manifest" href="/manifest.json" />
-
-        {/* Icons */}
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/180.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/icons/152.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/icons/120.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/icons/76.png" />
+        <title>Today Notes</title>
       </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
